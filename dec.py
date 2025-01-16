fe21525930d29ce75a22443e3566cd79a6a22910
Filipe <filipelisboaalves@gmail.com>
2025-01-16 20:07:26 -0300

    caminho soffice.exe tentativa 8

--- dec.py
+++ dec.py
@@ -485,7 +485,7 @@ if st.session_state["nova_dec"] == False:
         def docx_para_pdf():
             
            # LIBRE_OFFICE = os.getenv("LIBRE_OFFICE", ".\\LibreOffice\\program\\soffice.exe")
-            LIBRE_OFFICE = os.path.join(".", "LibreOffice", "program", "soffice.exe")
+            LIBRE_OFFICE = os.path.join("LibreOffice", "program", "soffice.exe")
 
             arquivo_de_entrada = f'{caminho_dec_docx}'
             pasta_destino = 'DECS'
